832dc2c26fb3bd0bbf2fc08d3c1c09cd2c2715ba
abuhamam2015929-netizen <abuhamam2015929@gmail.com>
2026-09-08 05:48:12 +0300

    Update MainActivity.kt

--- app/src/main/java/com/ahmed/souqalmawashi/MainActivity.kt
+++ app/src/main/java/com/ahmed/souqalmawashi/MainActivity.kt
@@ -3,6 +3,7 @@ package com.ahmed.souqalmawashi
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.material.icons.filled.Home
